5e28e83ffacfc3ca804228aa7f1e5be52c094b68
Daniel Tan <dtch1997@stanford.edu>
2021-02-17 23:35:37 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Speech Command
   Speech Command Recognizer from Microphone in browser using javascript library tensorflowjs
-  
-## Live Demo
-**[https://bensonruan.com/speech-recognition-with-tensorflow-js/](https://bensonruan.com/speech-recognition-with-tensorflow-js/)**
 
 ## Installing
 Clone this repository to your local computer
@@ -11,14 +8,11 @@ git https://github.com/bensonruan/Speech-Command.git
 ```
 Point your localhost to the cloned root directory
 
-Browse to http://localhost/index.html 
+Browse to http://localhost/index.html
 
 
 ## Start Detecting
-* Turn on the microphone switch and allowing the browser to access your microphone 
-
-## Supported Command words
-The 20 item vocaulbary, consisting of: 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'up', 'down', 'left', 'right', 'go', 'stop', 'yes', and 'no', in addition to 'background_noise' and 'unknown'.
+* Turn on the microphone switch and allowing the browser to access your microphone
 
 ## Library
 * [jquery](https://code.jquery.com/jquery-3.3.1.min.js) - JQuery
--- js/speech_command.js
+++ js/speech_command.js
@@ -2,9 +2,11 @@ let recognizer;
 let words;
 let wordList;
 let modelLoaded = false;
+let startTime;
+let d = new Date();
 
 $( document ).ready(function() {
-    wordList = ["zero","one","two","three","four","five","six","seven","eight","nine", "yes", "no", "up", "down", "left", "right", "stop", "go"];
+    wordList = ["Negative", "Positive"]
     $.each(wordList, function( index, word ) {
         if (!word.startsWith('_')){
             $("#candidate-words").append(`<span class='candidate-word col-md-2 col-sm-3 col-3' id='word-${word}'>${word}</span>`);
@@ -22,20 +24,33 @@ $("#audio-switch").change(function() {
     }
     else {
         stopListening();
-    }   
+    }
 });
 
 function loadModel(){
-    $(".progress-bar").removeClass('d-none'); 
+    $(".progress-bar").removeClass('d-none');
     // When calling `create()`, you must provide the type of the audio input.
     // The two available options are `BROWSER_FFT` and `SOFT_FFT`.
     // - BROWSER_FFT uses the browser's native Fourier transform.
     // - SOFT_FFT uses JavaScript implementations of Fourier transform (not implemented yet).
-    recognizer = speechCommands.create('BROWSER_FFT');  
+    const URL = "https://cs329s-edge.s3.us-east-2.amazonaws.com/tm-my-audio-model/";
+    const checkpointURL = URL + "model.json"; // model topology
+    const metadataURL = URL + "metadata.json"; // model metadata
+    console.log("Loading model from " + URL);
+    startTime = d.getTime();
+    recognizer = speechCommands.create(
+            "BROWSER_FFT", // fourier transform type, not useful to change
+            undefined, // speech commands vocabulary feature, not useful for your models
+            checkpointURL,
+            metadataURL);
     Promise.all([
         // Make sure that the underlying model and metadata are loaded via HTTPS requests.
         recognizer.ensureModelLoaded()
       ]).then(function(){
+        console.log("Model loaded in " + ((d.getTime() - startTime) / 1000) + " seconds");
+        console.log(recognizer.modelInputShape());
+        console.log(recognizer.params().sampleRateHz);
+        console.log(recognizer.params().fftSize);
         $(".progress-bar").addClass('d-none');
         words = recognizer.wordLabels();
         $.each(words, function( index, word ) {
@@ -65,7 +80,7 @@ function startListening(){
         setTimeout(() => {
             $("#word-"+scores[0].word).removeClass('candidate-word-active');
         }, 2000);
-    }, 
+    },
     {
         probabilityThreshold: 0.70
     });
